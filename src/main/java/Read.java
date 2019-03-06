1e9a1439fd2e36175e9cbbdc6e642b1183fb6c97
Daniel Benedek <benedekdj@cardiff.ac.uk>
2019-03-06 21:43:28 +0000

    Creating the reader algorithm for future reader instances.

---
+++ src/main/java/Read.java
@@ -0,0 +1,36 @@
+import java.io.Reader;
+import java.nio.file.Path;
+import java.util.List;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+
+public class Read {
+
+    public List<String[]> readFileAndParse(Path csvFile) {
+        List<String[]> records=null;
+        try {
+            CSVParser parser = new CSVParserBuilder()
+                    .withSeparator(',')
+                    .withIgnoreQuotations(true)
+                    .build();
+            Reader reader = Files.newBufferedReader(csvFile);
+            CSVReader csvReader = new CSVReaderBuilder(reader)
+                    .withSkipLines(1)
+                    .withCSVParser(parser)
+                    .build();
+            records = csvReader.readAll();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+}
